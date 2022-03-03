7a116bed6ef4123a29b1bc29672a9abb08f45d12
Aldhafiri70 <thelordshoes@Gmail.com>
2022-03-04 01:39:11 +0300

    Initial Commit

---
+++ Cw-2/Cw_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Cw_2App.swift
+//  Cw-2
+//
+//  Created by Abdulrazq Al dhafiri on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cw_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
